b2fc5671fec79e06f8f0dc5c83c608bed0f5e8b9
Ramis Wachtler <ramis.wachtler@outlook.com>
2015-01-15 10:50:28 +0100

    Added Codemirror editor
    ~ modified main.js and main_template to fit Codemirror requirements
    ~ fixed some style issues
    ~ modified they way of accessing HTML, JS and CSS values. (Codemirror)

--- public/js/main.js
+++ public/js/main.js
@@ -5,22 +5,46 @@ $(document).ready(function(){
     var result = $("#result");
     var js = $("#js").val();
     var iframe = $("#result_frame");
-
+    var htmlTextArea = document.getElementById("html");
+    var cssTextArea = document.getElementById("css");
+    var jsTextArea = document.getElementById("js");
+    var htmlCodeMirror = CodeMirror(function(elt) {
+        htmlTextArea.parentNode.replaceChild(elt, htmlTextArea);
+    },
+        {
+            value: htmlTextArea.value,
+            lineNumbers: true
+        });
+    var cssCodeMirror = CodeMirror(function(elt) {
+            cssTextArea.parentNode.replaceChild(elt, cssTextArea);
+        },
+        {
+            value: cssTextArea.value,
+            lineNumbers: true,
+            mode:  "css"
+        });
+    var jsCodeMirror = CodeMirror(function(elt) {
+            jsTextArea.parentNode.replaceChild(elt, jsTextArea);
+        },
+        {
+            value: jsTextArea.value,
+            lineNumbers: true,
+            mode:  "javascript"
+        });
     $("#run").click(function(){
         iframe.contents().find("head").empty();
         iframe.contents().find("head").append("<script src='/public/js/jquery/jquery.min.js'></script>");
-        var htmlInput = $("#html").val();
-        var css = $("#css").val();
-        var js = $("#js").val();
-        if(css !== undefined){
+        var htmlInput = htmlCodeMirror.getValue();
+        //var css = $("#css").val();
+        //var js = $("#js").val();
+        if(cssCodeMirror.getValue() !== undefined){
 
-            var data=[css];
+            var data=[cssCodeMirror.getValue()];
 
 
             var $style=$('<style>');
 
             $.each( data, function(i, item){
-                console.log(item);
                 $style.append(item + ';');
             })
                 iframe.contents().find("head").append($style)
@@ -29,9 +53,18 @@ $(document).ready(function(){
         }
         iframe.contents().find("body").html(htmlInput);
 
-        if(js !== undefined){
+        if(jsCodeMirror.getValue() !== undefined){
 
-            var script = "<script type='text/javascript'> function iFrameLoad() { var iframe = document.getElementById('result_frame'); var innerDoc = iframe.contentDocument || iframe.contentWindow.document; "+js.replace(/document\./g, "innerDoc.")+"}</script><script type='text/javascript'>$(document).ready(function () {iFrameLoad();});</script>";
+            var script = "<script type='text/javascript'> " +
+                "function iFrameLoad() { " +
+                    "var iframe = document.getElementById('result_frame');" +
+                    " var innerDoc = iframe.contentDocument || iframe.contentWindow.document; " +
+                    jsCodeMirror.getValue().replace(/document\./g, "innerDoc.")+"" +
+                "}" +
+                "</script>" +
+                "<script type='text/javascript'>" +
+                    "$(document).ready(function () {iFrameLoad();});" +
+                "</script>";
 
             iframe.contents().find("head").append(script);
         }
